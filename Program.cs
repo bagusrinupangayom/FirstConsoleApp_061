e6aef0ae907104fabe2219f56d13d9dcf3924a84
Bagus Rinu Pangayom <bagusrp2001@gmail.com>
2021-10-07 09:55:45 +0700

    Added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp_061
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bagus Rinu Pangayom 20190140061");
+            Console.ReadLine();
+        }
+    }
+}
